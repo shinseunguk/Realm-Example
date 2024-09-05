435b1061c6c3206dd3f99b8a444065ada6d9def0
신승욱 <ukseung.dev@incross.com>
2024-09-05 16:18:43 +0900

    Initial Commit

---
+++ Realm-Example/Realm_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_ExampleApp.swift
+//  Realm-Example
+//
+//  Created by ukseung.dev on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Realm_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
